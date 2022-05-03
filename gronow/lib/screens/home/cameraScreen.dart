9ae20bf0e0f25055fa77120fbd5e69bab4274651
PauloGSP <paulogspereira@ua.pt>
2022-05-03 19:44:07 +0100

    filhas

---
+++ gronow/lib/screens/home/cameraScreen.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraScreen> {
+  final qrKey = GlobalKey(debugLabel: "QR");
+  QRViewController? controller;
+  Barcode? barcode;
+
+  @override
+  void dispose() {
+    controller?.dispose(); // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  void reassemble() async {
+    // TODO: implement reassemble
+    super.reassemble();
+    if (Platform.isAndroid) {
+      await controller!.pauseCamera();
+    }
+    controller!.resumeCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+        child: Scaffold(
+          body: Stack(
+            alignment: Alignment.center,
+            children: <Widget>[
+              buildQrView(context),
+              Positioned(bottom: 10, child: buildResult()),
+              Positioned(top: 10, child: buildButtons())
+            ],
+          ),
+        ),
+      );
+  Widget buildQrView(BuildContext context) => QRView(
+        key: qrKey,
+        onQRViewCreated: onQRViewCreated,
+        overlay: QrScannerOverlayShape(),
+      );
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() => this.controller = controller);
+    controller.scannedDataStream
+        .listen((barcode) => setState(() => this.barcode = barcode));
+  }
+
+  Widget buildResult() => Container(
+      padding: EdgeInsets.all(12),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(24), color: Colors.white24),
+      child: Text(barcode != null ? 'Result : ${barcode!.code}' : 'Scan a code',
+          maxLines: 3));
+
+  Widget buildButtons() => Container(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white24,
+      ),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          IconButton(
+            onPressed: () async {
+              await controller?.toggleFlash();
+              setState(() {});
+            },
+            icon: FutureBuilder<bool?>(
+                future: controller?.getFlashStatus(),
+                builder: (context, snapshot) {
+                  if (snapshot.data != null) {
+                    return Icon(
+                        snapshot.data! ? Icons.flash_on : Icons.flash_off);
+                  } else {
+                    return Container();
+                  }
+                }),
+          )
+        ],
+      ));
+}
