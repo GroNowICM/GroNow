b2d2d6c25fed58592d74e540864d8db79361c442
Fábio Martins <fabio.m@ua.pt>
2022-05-04 00:18:55 +0100

    added new colors

--- gronow/lib/styles/colors.dart
+++ gronow/lib/styles/colors.dart
@@ -11,6 +11,8 @@ class AppColors {
   AppColors._();
 
   static const primaryColor = Color(0xff53B175);
+  static const secondaryColor = Color.fromARGB(255, 118, 247, 165);
   static const darkGrey = Color(0xff7C7C7C);
+  static const lightGrey = Color.fromARGB(255, 240, 240, 240);
   static const background = Color.fromARGB(255, 147, 196, 161);
 }
